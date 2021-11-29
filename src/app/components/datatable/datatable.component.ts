e4ef1f5bf7d27b0847430cf7ef4c9be49b233c88
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-11-29 14:22:09 -0300

    sweetalert cantidad de items a mostrar

--- src/app/components/datatable/datatable.component.ts
+++ src/app/components/datatable/datatable.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import Swal  from 'sweetalert2'
 
 @Component({
   selector: 'app-datatable',
@@ -22,10 +23,12 @@ export class DatatableComponent implements OnInit, OnDestroy {
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 5,
+      lengthMenu: [5, 10, 15, 20],
       language: {
         url: '//cdn.datatables.net/plug-ins/1.11.3/i18n/es-cl.json'
       }
     };
+    
     this.httpClient.get('https://dummy.restapiexample.com/api/v1/employees').pipe(
       map( (resp: any) => {
         return resp.data;
@@ -34,6 +37,13 @@ export class DatatableComponent implements OnInit, OnDestroy {
       this.personas = resp;
       console.log(this.personas);
       this.dtTrigger.next();
+    },
+    error => {
+      Swal.fire({
+        icon: 'error',
+        title: error.error.message 
+      });
+
     });
   }
 
